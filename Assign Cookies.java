6866394972f43f73d32b405f08a616042f1011cb
pathakharsh18 <137424790+pathakharsh18@users.noreply.github.com>
2024-11-12 16:16:08 +0530

    commit
    commit

--- Assign Cookies.java
+++ Assign Cookies.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int c=0,j=0,i=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                c++;
+                i++;
+            }
+            j++;
+        }
+        return c;
+    }
+}
